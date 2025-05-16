66a7f26d06918876b8619b81cd9286e4537b6d26
unknown <mirandaelcid27@gmail.com>
2025-05-16 17:50:31 +0800

    05162025 Last Update

---
+++ BBIS.Domain/Models/DonorCounseling.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBIS.Domain.Models
+{
+    public class DonorCounseling
+    {
+        public Guid DonorMedicalHistoryId { get; set; }
+        public Guid DonorRegistrationId { get; set; }
+        public int MedicalQuestionnaireId { get; set; }
+        public string Answer { get; set; }
+        public string Remarks { get; set; }
+
+        public virtual DonorRegistration DonorRegistration { get; set; }
+        public virtual MedicalQuestionnaire MedicalQuestionnaire { get; set; }
+    }
+}
+
